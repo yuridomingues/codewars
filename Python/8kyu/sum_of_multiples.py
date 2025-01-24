6189284dbd1b11b6c8fcd7f986e2b691919656b5
yuridomingues <yuridomingues.contato@gmail.com>
2025-01-24 13:00:47 -0300

    fix: removed trailing spaces

--- Python/8kyu/sum_of_multiples.py
+++ Python/8kyu/sum_of_multiples.py
@@ -1,13 +1,15 @@
 def sum_mul(n, m):
-    total_sum = 0
-    if (n == 0 or n < 0 or m == 0 or m < 0 ):
+
+    if (n <= 0 or m <= 0):
         return "INVALID"
+
     if (n == m or n > m):
         return 0
 
-    else:
-        n = n + n
-        total_sum += n
-        if n <= m:
-            return total_sum
+    total_sum = 0
+
+    for multiple in range(n, m, n):
+        total_sum += multiple
+
+    return total_sum
         
