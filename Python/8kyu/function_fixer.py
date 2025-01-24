5b2c94512cb386b79d86492e667e6605213987b1
yuridomingues <yuridomingues.contato@gmail.com>
2025-01-24 11:51:30 -0300

    feat(kata): basic function fixer '

---
+++ Python/8kyu/function_fixer.py
@@ -0,0 +1,22 @@
+"""
+This module contains a function that adds 5 to a given number.
+
+Functions:
+    add_five(num): Adds 5 to the given number and returns the result.
+"""
+def add_five(num):
+    """
+    Adds 5 to the given number.
+
+    Parameters:
+    num (int): The number to which 5 will be added.
+
+    Returns:
+    int: The result of adding 5 to the given number.
+
+    Example:
+    >>> add_five(3)
+    8
+    """
+    total = num + 5
+    return total
